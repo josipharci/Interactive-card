5bb473d0d6aa82e2c8144c39e6a8ef92709e6ef3
Josip Harci <harcijosip99@gmail.com>
2022-12-01 22:11:22 +0100

    Update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,36 @@
-var name = document.getElementById('name');
-var card_number = document.getElementById('card_number');
-var MM = document.getElementById('MM');
-var YY = document.getElementById('YY');
-var CVC = document.getElementById('CVC');
-var from = document.getElementById('from');
+const form = document.getElementById('form');
+const names = document.getElementById('name');
+const cardNumber= document.getElementById('card_number');
+const MM = document.getElementById('MM');
+const YY= document.getElementById('YY');
+const CVC= document.getElementById('CVC');
 
-var card_number_error = document.getElementById('card_number_error');
-var MM_YY_error = document.getElementById('MM_YY_error');
-var CVC_error = document.getElementById('CVC_error');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+})
+
+function checkInputs(){
+   const namesValue = names.value.trim();
+   const cardNumberValue = cardNumber.value.trim();
+   const MMValue = MM.value.trim();
+   const YYValue = YY.value.trim();
+   const CVCValue = CVC.value.trim();
+
+   if(namesValue === ''){
+        setErrorFor(names,'name cennot be blank');
+   }else{
+        setSuccessFor(names);
+   }
+
+   function setErrorFor(input,message){
+     const formContorl = input.parentElement;
+     const span = formContorl.querySelector('span');
+
+     span.innerText = message;
+     formContorl.className('form-control error');
+   }
+}
 
