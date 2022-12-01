ac89ec80ec5a5f851c72f50e3d1936a5419780c9
Josip Harci <harcijosip99@gmail.com>
2022-12-01 09:00:40 +0100

    Update

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+const name = document.getElementById('name');
+const card_number = document.getElementById('card_number');
+const MM = document.getElementById('MM');
+const YY = document.getElementById('YY');
+const CVC = document.getElementById('CVC');
+const from = document.getElementById('from');
+
+
