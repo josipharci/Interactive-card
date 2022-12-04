88be3f1a5b4c75392279f5839424124c15cce258
Josip Harci <harcijosip99@gmail.com>
2022-12-04 23:52:30 +0100

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,24 +12,54 @@ const fullNameError = document.getElementById('fullNameError');
 const cardNumberError = document.getElementById('cardNumberError');
 const MMYYNumberError = document.getElementById('MMYYNumberError');
 const CVCNumberError = document.getElementById('CVCNumberError');
-const sub = document.getElementsByClassName('sub');
 
 function cardAction(){
 
-    if(cardNumber.value === ''){
-        cardNumberError.innerHTML = 'Wrong format, number only';
-    }
-    if(MMNumber.value,YYNumber.value === ''){
-        MMYYNumberError.innerHTML = "Can't be blank";
-    }
-    if(CVCNumber.value === ''){
-        CVCNumberError.innerHTML = "Can't be blank";
-    }
+    
+    
+        if(cardNumber.value === ""){
+            cardNumberError.innerHTML = "Wrong format, number only";
+        }else{
+            cardNumberError.innerHTML = "";
+        }
+    
+        if(MMNumber.value === ""){
+            MMYYNumberError.innerHTML = "Can't be blank";
+        }else{
+            MMYYNumberError.innerHTML = ""; 
+        }
+    
+        if(YYNumber.value === ""){
+            CVCNumberError.innerHTML = "Can't be blank";
+        }else{
+            CVCNumberError.innerHTML = ""; 
+        }
+        
+        if(cardNumber.value == ""){
+            cardFrontNum.innerHTML = "0000 0000 0000 0000";
+        }else{
+            cardFrontNum.innerHTML = cardNumber.value;
+        }
 
-    cardFrontNum.innerHTML = cardNumber.value;
-    cardFrontName.innerHTML= fullName.value;
-    cardBackNum.innerHTML= CVCNumber.value;
-    cardFrontMMYY.innerHTML= MMNumber.value + '/' + YYNumber.value;
+        if(fullName.value == ""){
+            cardFrontName.innerHTML = "Jane Appleseed";
+        }else{
+            cardFrontName.innerHTML= fullName.value;
+        }
+
+        if(CVCNumber.value == ""){
+            cardBackNum.innerHTML = "000";
+        }else{
+            cardBackNum.innerHTML= CVCNumber.value;
+        }
+        
+        if(MMNumber.value,YYNumber.value == ""){
+            cardFrontMMYY.innerHTML = "00/00";
+        }else{
+            cardFrontMMYY.innerHTML= MMNumber.value + '/' + YYNumber.value;
+        }
+
+        
 }
 
 submit.addEventListener('click', cardAction);
