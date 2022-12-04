10a823619b850abeb65a482da9bdc12b0e5d6169
Josip Harci <harcijosip99@gmail.com>
2022-12-04 22:43:01 +0100

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,29 @@ const submit = document.getElementById('submit');
 const cardFrontNum = document.getElementById('cardFrontNum');
 const cardFrontName = document.getElementById('cardFrontName');
 const cardFrontMMYY = document.getElementById('cardFrontMMYY');
-const cardBackNum = document.getElementById('cardBackNum');
+const fullNameError = document.getElementById('fullNameError');
+const cardNumberError = document.getElementById('cardNumberError');
+const MMYYNumberError = document.getElementById('MMYYNumberError');
+const CVCNumberError = document.getElementById('CVCNumberError');
+const sub = document.getElementsByClassName('sub');
 
 function cardAction(){
+
+    if(cardNumber.value === ''){
+        cardNumberError.innerHTML = 'Wrong format, number only';
+    }
+    if(MMNumber.value,YYNumber.value === ''){
+        MMYYNumberError.innerHTML = "Can't be blank";
+    }
+    if(CVCNumber.value === ''){
+        CVCNumberError.innerHTML = "Can't be blank";
+    }
+
     cardFrontNum.innerHTML = cardNumber.value;
     cardFrontName.innerHTML= fullName.value;
     cardBackNum.innerHTML= CVCNumber.value;
     cardFrontMMYY.innerHTML= MMNumber.value + '/' + YYNumber.value;
 }
-submit.addEventListener('click',cardAction);
+
+submit.addEventListener('click', cardAction);
+
